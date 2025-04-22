74cdfb602a177100b8afc61ea131d650949f1172
tjjd4 <bbb@ssstjjd4.local>
2025-04-22 16:44:58 +0800

    feat: move register tools function to base class

--- src/agent/base_ag2_agent.py
+++ src/agent/base_ag2_agent.py
@@ -1,7 +1,34 @@
-from abc import abstractmethod
+from typing import List, Literal
 from autogen import ConversableAgent
+from autogen.mcp import create_toolkit
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.sse import sse_client
+from mcp.client.stdio import stdio_client
 
 class BaseAgent(ConversableAgent):
 
     def __init__(self, name: str, system_message: str, **kwargs):
         super().__init__(name=name, system_message=system_message, **kwargs)
+
+    async def register_mcp_tools(self, mcp_cmd: str, mcp_args: List[dict], agents: List[ConversableAgent], mode: Literal["stdio", "sse"] = "stdio"):
+        server_params = StdioServerParameters(
+            command=mcp_cmd,
+            args=mcp_args,
+        )
+
+        if mode == "stdio":
+            async with stdio_client(server_params) as (read, write), ClientSession(read, write) as session:
+                await session.initialize()
+                toolkit = await create_toolkit(session)
+                toolkit.register_for_llm(self)
+                for agent in agents:
+                    toolkit.register_for_execution(agent)
+        elif mode == "sse":
+            async with sse_client(server_params) as (read, write), ClientSession(read, write) as session:
+                await session.initialize()
+                toolkit = await create_toolkit(session)
+                toolkit.register_for_llm(self)
+                for agent in agents:
+                    toolkit.register_for_execution(agent)
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
--- src/agent/planner_agent.py
+++ src/agent/planner_agent.py
@@ -1,14 +1,12 @@
-from typing import List
-from autogen import LLMConfig, UserProxyAgent, ConversableAgent
-from autogen.mcp import create_toolkit
-from mcp import ClientSession, StdioServerParameters
-from mcp.client.sse import sse_client
-from mcp.client.stdio import stdio_client
+from autogen import LLMConfig, UserProxyAgent
+import logging
 
 from .base_ag2_agent import BaseAgent
 from src.utils.prompt import LLM_PROMPTS
 from src.utils.config import Config
 from src.utils.pydantic_types import Intent
+
+logger = logging.getLogger(__name__)
 
 ollama_llm_config = LLMConfig(
     model= "mistral-nemo:12b-instruct-2407-q2_K",
@@ -27,19 +25,6 @@ class PlannerAgent(BaseAgent):
         system_message = LLM_PROMPTS["PLANNER_AGENT_PROMPT"]
         super().__init__(name="PlannerAgent", system_message=system_message, llm_config=ollama_llm_config, **kwargs)
 
-    async def register_mcp_tools(self, mcp_cmd: str, mcp_args: List[dict], agents: List[ConversableAgent]):
-        server_params = StdioServerParameters(
-            command=mcp_cmd,
-            args=mcp_args,
-        )
-
-        async with stdio_client(server_params) as (read, write), ClientSession(read, write) as session:
-            await session.initialize()
-            toolkit = await create_toolkit(session)
-            toolkit.register_for_llm(self)
-            for agent in agents:
-                toolkit.register_for_execution(agent)
-
 
     async def plan(self, user_input: str, intent: Intent) -> list[str]:
         user_agent = UserProxyAgent(
@@ -51,7 +36,7 @@ class PlannerAgent(BaseAgent):
         )
 
         await self.register_mcp_tools(mcp_cmd="node", mcp_args=["src/mcp/evm-mcp-server/build/index.js"], agents=[user_agent])
-        print("MCP tools registered")
+        logger.info("[PlannerAgent] MCP tools registered")
         result = user_agent.initiate_chat(
             recipient=self,
             message=f"User input: {user_input}\nIntent: {intent}",
